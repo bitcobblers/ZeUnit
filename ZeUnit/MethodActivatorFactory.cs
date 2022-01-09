eaf3d111cff49badb9df1dda97b8edf4b5faac9d
Sergei Golos <sergei.golos@gmail.com>
2022-01-09 00:33:33 -0500

    moved classes to own files

---
+++ ZeUnit/MethodActivatorFactory.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ZeUnit;
+
+public static class MethodActivatorFactory
+{
+    public static IEnumerable<IZeTestActivator> Get(MethodInfo method)
+    {
+        var activators = method.GetCustomAttributes()
+            .Where(n => n.GetType().IsAssignableTo(typeof(ZeActivatorAttribute)))
+            .Select(n => (ZeActivatorAttribute)n)
+            .Select(n => n.Activator)
+            .Select(n => (IZeTestActivator)Activator.CreateInstance(n));
+
+        return activators.Any()
+            ? activators
+            : new[] { new CoreTestActivator() };
+    }
+}
