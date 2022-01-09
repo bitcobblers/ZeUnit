92cdd936436bb01e541e22641c72b144e6a481b7
Sergei Golos <sergei.golos@gmail.com>
2022-01-08 21:43:59 -0500

    simple implementation of ITestDiscoverer

---
+++ ZeUnit.Runner.VisualStudio/VisualStudioTestDiscovery.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using System.Reflection;
+
+namespace ZeUnit.Runner.VisualStudio
+{
+    [FileExtension(".dll")]
+    [FileExtension(".exe")]
+    [DefaultExecutorUri("ZeUnit.Runner.VisualStudio.VisualStudioZeUnitExecutor")]
+    public class VisualStudioTestDiscovery : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            Console.WriteLine("test");
+            foreach (var source in sources)
+            {
+                var discovery = new ZeDiscovery()
+                    .FromAssembly(Assembly.LoadFrom(source));
+
+                foreach (var test in discovery)
+                {
+                    discoverySink.SendTestCase(new TestCase(
+                        $"{test.Class.FullName}::{test.Method.Name}",
+                        new Uri("ZeUnit.Runner.VisualStudio.VisualStudioZeUnitExecutor"),
+                        source));
+                }
+            }
+
+        }
+    }
+}
--- ZeUnit.Runner.VisualStudio/VisualStudioZeUnitExecutor.cs
+++ ZeUnit.Runner.VisualStudio/VisualStudioZeUnitExecutor.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
 
 namespace ZeUnit.Runner.VisualStudio
 {
-    public class VisualStudioRunner : ITestDiscoverer, ITestExecutor
-    {
-        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
-        {
-            Console.WriteLine("test");            
-        }
-
+    [ExtensionUri("ZeUnit.Runner.VisualStudio.VisualStudioZeUnitExecutor")]
+    public class VisualStudioZeUnitExecutor : ITestExecutor
+    { 
         public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
         {
             Console.WriteLine("test");
