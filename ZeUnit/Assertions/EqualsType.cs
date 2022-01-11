391521c428190937b46bea2a8b4e376667287dce
Sergei Golos <sergei.golos@gmail.com>
2022-01-11 11:36:44 -0500

    file cleanup

--- ZeUnit/Assertions/EqualsType.cs
+++ ZeUnit/Assertions/EqualsType.cs
@@ -1,16 +1,5 @@
 ﻿namespace ZeUnit.Assertions;
 
-
-public static class EqualsEnumerable
-{
-    public static ZeResult IsNotEmpty<TType>(this ZeResult result, IEnumerable<TType> enumerable)
-    {        
-        return result.Assert(enumerable.Any()
-            ? new Successful($"Enumerable contains items.")
-            : new Failed($"Enumerable expected to have elements, but none were found."));
-    }
-}
-
 public static class EqualsType
 {
     public static ZeResult IsType(this ZeResult result, Type expectedType, object actual)
--- ZeUnit/IZeRunner.cs
+++
@@ -1,8 +0,0 @@
-﻿//// See https://aka.ms/new-console-template for more information
-//namespace ZeUnit
-//{
-//    public interface IZeRunner
-//    {
-//        void Run(ZeTest test);
-//    }
-//}
