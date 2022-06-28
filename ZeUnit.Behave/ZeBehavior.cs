e1f529ed32e5fcd579b1ae7df743320af31318af
Sergei Golos <sergei.golos@gmail.com>
2022-06-27 23:00:06 -0400

    moved class to its own file

--- ZeUnit.Behave/ZeBehavior.cs
+++ ZeUnit.Behave/ZeBehavior.cs
@@ -1,24 +1,5 @@
 ﻿namespace ZeUnit.Behave
 {
-    public class BehaviorResult : ZeResult
-    {
-        public BehaviorResult(string message, Action action) 
-        {
-            var attempted = new AssertPassed(message);
-            this.Assert(attempted);
-            
-            try
-            {
-                action();
-            }
-            catch (Exception ex)
-            {
-                attempted.Status = ZeStatus.Failed;
-                this.Assert(new AssertError(ex));
-            }            
-        }
-    }
-
     public abstract class ZeBehavior
     {
         protected ZeResult Given(string message, Action action = null)
@@ -41,7 +22,7 @@
         {
             return new BehaviorResult($"Then: '{message}'", () =>
             {
-                (action ?? (() => { return new ZeResult(); }))();
+                (action ?? (() => { return Ze.Is; }))();
             });
         }      
     }
