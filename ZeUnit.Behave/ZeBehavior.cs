98163e01228fa80976fddd252e4b85a531cfe834
Sergei Golos <sergei.golos@gmail.com>
2022-06-28 11:44:24 -0400

    some structure around DI, not sure this is going to work correctly yet

--- ZeUnit.Behave/ZeBehavior.cs
+++ ZeUnit.Behave/ZeBehavior.cs
@@ -2,33 +2,105 @@
 
 namespace ZeUnit.Behave
 {
-    public abstract class ZeContainerBehavior
+    public class ContainerBehaviorActionResult<TType> : ZeResult
+        where TType : IBehaviorAction
+    {
+        public ContainerBehaviorActionResult(Container container, string errorMessage) 
+        {            
+            var instance = container.TryGetInstance<TType>();
+            if (instance != null)
+            {
+                foreach (var assert in new BehaviorActionResult(instance, container))
+                {
+                this.Assert(assert);
+                }
+                    
+            }
+
+            this.Assert(new AssertError(
+                new Exception(errorMessage)));
+            
+        }
+    }
+
+    public class BehaviorActionResult : BehaviorResult
+    {
+        public BehaviorActionResult(IBehaviorAction action, Container container) : base(action.Message, () => {
+            var registry = action.Do();
+            container.Configure(registry);
+        })
+        {
+        }
+    }
+
+    public abstract class ZeContainerBehavior : IDisposable
     {
         private Container container = new Container(r => { });
-
-        protected ZeResult Given<TGiven>(string message = "") 
-            where TGiven : ZeGiven
+        protected ZeResult Given<TGiven>() 
+            where TGiven : ZeGiven, IBehaviorAction
         {
-            return Ze.Is;
+            var instance = container.TryGetInstance<TGiven>();
+            if (instance != null)
+            {
+                return new BehaviorActionResult(instance, container);
+            }
+
+            return Ze.Is.Assert(new AssertError(
+                new Exception($"Could not load Given type {nameof(TGiven)} from container.")));
         }
 
-        protected ZeResult When<TWhen>(string message = "")
-         where TWhen : ZeWhen
+        protected ZeResult Given(ZeGiven given)            
         {
-            return Ze.Is;
+            return new BehaviorActionResult(given, container);
         }
 
-        protected ZeResult Then<TThen>(string message = "")
+        protected ZeResult When<TWhen>()
+         where TWhen : ZeWhen, IBehaviorAction
+        {
+            var instance = container.TryGetInstance<TWhen>();
+            if (instance != null)
+            {
+                return this.When(instance);
+            }
+
+            return Ze.Is.Assert(new AssertError(
+                new Exception($"Could not load When type {nameof(TWhen)} from container.")));
+        }
+
+        protected ZeResult When(ZeWhen when)
+        {
+            return new BehaviorActionResult(when, container);
+        }
+
+
+        protected ZeResult Then<TThen>()
             where TThen : ZeThen
         {
+            var instance = container.TryGetInstance<TThen>();
+            if (instance != null)
+            {
+                return this.Then(instance);
+            }
+
+            return Ze.Is.Assert(new AssertError(
+                new Exception($"Could not load Then type {nameof(TThen)} from container.")));
+        }
+
+        protected ZeResult Then(ZeThen then)            
+        {
             return Ze.Is;
+        }
+
+        public void Dispose()
+        {
+            this.container.Dispose();
         }
     }    
 
     public interface IBehaviorAction
     {
         string Message { get; }
-        IEnumerable<ServiceRegistry> Do();
+        ServiceRegistry Do();
     }
 
     public interface IBehaviorTest
@@ -38,58 +110,45 @@ namespace ZeUnit.Behave
         ZeResult Do();
     }
 
-    public class ZeGiven : IBehaviorAction
-    {
-        private readonly Action action;
+    public abstract class ZeBehaviorAction : IBehaviorAction {
+                        
+        public string Message { get; protected set; }
 
-        public ZeGiven(string message, Action action)
+        protected abstract void Do(ServiceRegistry registry);
+
+        public ServiceRegistry Do()
         {
-            this.action = action;
+            var registry = new ServiceRegistry();
+            this.Do(registry);
+            return registry;
+        }
+    }
+
+    public abstract class ZeGiven : ZeBehaviorAction, IBehaviorAction
+    {                
+        public ZeGiven(string message)
+        {            
             Message = $"GIVEN:: {message}";
-        }
-
-        public string Message { get; }
-
-        public virtual IEnumerable<ServiceRegistry> Do()
-        {
-            // (action ?? (() => { }))();
-            yield break;
-        }
-
-    }
-    public class ZeThen : IBehaviorTest
-    {
-        private readonly Func<ZeResult> fn;
-
-        public ZeThen(string message, Func<ZeResult> fn)
-        {
-            Message = $"THEN:: '{message}'";
-            this.fn = fn;
-        }
-
-        public string Message { get; }
-
-        public virtual ZeResult Do()
-        {
-            return (fn ?? (() => { return Ze.Is; }))();
-        }
+        }       
     }
 
-    public class ZeWhen : IBehaviorAction
+    public abstract class ZeWhen : ZeBehaviorAction, IBehaviorAction
     {
-        private readonly Action action;
-
-        public ZeWhen(string message, Action action)
+        public ZeWhen(string message)
         {
-            this.action = action;
             this.Message = $"WHEN:: '{message}'";
         }
+    }
+
+    public abstract class ZeThen : IBehaviorTest
+    {        
+        public ZeThen(string message)
+        {
+            Message = $"THEN:: '{message}'";            
+        }
 
         public string Message { get; }
 
-        public virtual IEnumerable<ServiceRegistry> Do()
-        {
-            yield break;
-        }
-    }
+        public abstract ZeResult Do();
+    }    
 }
