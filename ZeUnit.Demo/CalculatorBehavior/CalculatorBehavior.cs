b4a2639050bb502a8e234f80f3d28b27b71962f2
Sergei Golos <sergei.golos@gmail.com>
2022-06-27 22:12:58 -0400

    cleaned up some syntax around behavior

--- ZeUnit.Demo/CalculatorBehavior/CalculatorBehavior.cs
+++ ZeUnit.Demo/CalculatorBehavior/CalculatorBehavior.cs
@@ -6,9 +6,25 @@ using System.Threading.Tasks;
 using ZeUnit.Behave;
 
 namespace ZeUnit.Demo.CalculatorBehavior
-{    
+{
+    public class CalculatorRegistry : ServiceRegistry
+    {
+        public CalculatorRegistry()
+        {
+            this.For<Calculator>().Use<Calculator>();
+        }
+    }
+
+    [LamarContainer(typeof(CalculatorRegistry))]
     public class CalculatorBehavior : ZeBehavior
     {
+        private readonly Calculator calulator;
+
+        public CalculatorBehavior(Calculator calulator)
+        {            
+            this.calulator = calulator;
+        }
+
         [InlineData(1, 2, 3)]
         [InlineData(2, 3, 5)]
         [InlineData(3, 4, 7)]
@@ -16,14 +32,13 @@ namespace ZeUnit.Demo.CalculatorBehavior
         [InlineData(5, 6, 11)]
         public IEnumerable<ZeResult> Addition(int a, int b, int expected)
         {
-            // context as regular variables no fuzzy logic;
-            var calulator = new Calculator();
+            // Context is the variables defined in this scope.
             int result = 0;
-
+            
             yield return Given($"Set first number to {a}.");
             yield return Given($"Set the second number to {b}");
-            yield return When("Add numbers", () => { result = calulator.Add(a, b); });
-            yield return Then("Expect ", z => z.Equal(result, expected));
+            yield return When("Number are added", () => { result = calulator.Add(a, b); });
+            yield return Then($"Expect {result} to be {expected}", z => z.Equal(result, expected));
         }
     }
 }
