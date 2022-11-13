3487c58c5209191632c47ed76353c88ff4241a3a
Sergei Golos <sergei.golos@gmail.com>
2022-11-13 00:49:16 -0500

    split the classes into different flies

---
+++ ZeUnit.Demo/FileTests/DirectoryInjectionTests.cs
@@ -0,0 +1,24 @@
+﻿namespace ZeUnit.Demo.FileTests
+{
+    public class DirectoryInjectionTests
+    {
+        [LoadDirectory("FileTests/test/")]
+        public ZeResult LoadedFromDirectory(string value)
+        {
+            return Ze.Is.NotEmpty(value);
+        }
+
+        [LoadDirectory("FileTests/test/", "sample.txt")]
+        public ZeResult LoadedFromDirectoryWithExtension(string value)
+        {
+            return Ze.Is.NotEmpty(value);
+        }
+
+        [LoadDirectory("FileTests/test/", "test.txt", "result.txt")]
+        public ZeResult LoadedFromDirectoryWithMultipleExtension(string value, string result)
+        {
+            return Ze.Is.Equal(result, value);
+        }
+
+    }
+}
