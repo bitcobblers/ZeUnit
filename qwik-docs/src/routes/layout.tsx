d7482e24e0f293d051087eb2d13575415fa128c5
Sergei Golos <sergei.golos@gmail.com>
2024-02-10 23:30:23 -0500

    cleaned up the formating a bit

--- qwik-docs/src/routes/layout.tsx
+++ qwik-docs/src/routes/layout.tsx
@@ -46,9 +46,9 @@ export default component$(() => {
               <Navigation />
             </div>
           </div>
-          <div class="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pl-8 lg:pr-0 xl:px-16 text-blue-600/100">
+          <article class="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pl-8 lg:pr-0 xl:px-16 text-slate-600/100">
             <Slot />
-          </div>
+          </article>
         </div>
       </div>
       </main>      
