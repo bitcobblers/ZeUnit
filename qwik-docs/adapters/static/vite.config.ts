0dddcf445e107095a0ba1100602f2ef86f9e4313
Sergei Golos <sergei.golos@gmail.com>
2024-02-11 18:07:30 -0500

    Qwik docs (#53)
    * added default qwik app
    
    * added the adins
    
    * removed default pages and started porting the template.  hero ported
    
    * Updated the names of static class Ze -> ZeGlobal and changed ZeResult to Ze for better looking and feel to the framework
    
    * Updated docs to rename ZeResult -> Ze
    
    * Cleaned up the hero with links configured
    
    * created place holders for the basic documentation to create. still wroking on formating the main content.
    
    * cleaned up the formating a bit
    
    * cleaned up pre formating
    
    * Renamed to FAct in docs
    
    * renamed Ze to Fact
    
    * changed the build scrip
    
    * scripted updated
    
    * removed file breaking the docs build
    
    * fixed typo
    
    * static code update
    
    * fixed vite build to point to sub directory for build path
    
    * updated origin and base settings
    
    * updated per thread on github
    
    * trying to fix the build issue
    
    * workround for build error
    
    * aligning the ssg and vite settings

--- qwik-docs/adapters/static/vite.config.ts
+++ qwik-docs/adapters/static/vite.config.ts
@@ -13,6 +13,7 @@ export default extendConfig(baseConfig, () => {
     plugins: [
       staticAdapter({
         origin: "https://bitcobblers.github.io/",
+        base: "/ZeUnit/",
       }),
     ],
   };
