10735ad4938665395209a201b8ccb605d5226179
Sergei Golos <sergei.golos@gmail.com>
2024-02-11 21:29:52 -0500

    updated linkings and changes

--- qwik-docs/src/routes/navigation.ts
+++ qwik-docs/src/routes/navigation.ts
@@ -1,44 +1,43 @@
 export const navigation = [
     {
       title: 'Introduction',
-      links: [
-        { title: 'Birds Eye View', href: '/ZeUnit/' },
-        { title: 'Getting Started', href: '/ZeUnit/docs/getting-started/' },                
+      links: [        
+        { title: 'Getting Started', href: '/docs/getting-started' },                
       ],
     },
     {
       title: 'Core Concepts',
       links: [        
-        { title: 'Test Basics', href: '/ZeUnit/docs/the-basic-test' },
-        { title: 'Class Activation', href: '/ZeUnit/docs/class-activation' },
-        { title: 'Method Activation', href: '/ZeUnit/docs/method-activation'},        
+        { title: 'Test Basics', href: '/docs/the-basic-test' },
+        { title: 'Class Activation', href: '/docs/class-activation' },
+        { title: 'Method Activation', href: '/docs/method-activation'},        
       ],
     },
     {
       title: 'Advanced Guides',
       links: [
-        { title: 'Test Suites & Lifecycles', href: '/ZeUnit/docs/test-suites-lifecylces' },
-        { title: 'Building Class Activators', href: '/ZeUnit/docs/building-class-activators' },
-        { title: 'Building Method Activators', href: '/ZeUnit/docs/building-method-activators' },
+        { title: 'Test Suites & Lifecycles', href: '/docs/test-suites-lifecylces' },
+        { title: 'Building Class Activators', href: '/docs/building-class-activators' },
+        { title: 'Building Method Activators', href: '/docs/building-method-activators' },
         
-        { title: 'Custom Reporting', href: '/ZeUnit/docs/custom-reporting' },        
+        { title: 'Custom Reporting', href: '/docs/custom-reporting' },        
       ],
     },
     // {
     //   title: 'API reference',
     //   links: [
-    //     { title: 'CacheAdvance.predict()', href: '/ZeUnit/docs/cacheadvance-predict' },
-    //     { title: 'CacheAdvance.flush()', href: '/ZeUnit/docs/cacheadvance-flush' },
-    //     { title: 'CacheAdvance.revert()', href: '/ZeUnit/docs/cacheadvance-revert' },
-    //     { title: 'CacheAdvance.regret()', href: '/ZeUnit/docs/cacheadvance-regret' },
+    //     { title: 'CacheAdvance.predict()', href: '/docs/cacheadvance-predict' },
+    //     { title: 'CacheAdvance.flush()', href: '/docs/cacheadvance-flush' },
+    //     { title: 'CacheAdvance.revert()', href: '/docs/cacheadvance-revert' },
+    //     { title: 'CacheAdvance.regret()', href: '/docs/cacheadvance-regret' },
     //   ],
     // },
     // {
     //   title: 'Contributing',
     //   links: [
-    //     { title: 'How to contribute', href: '/ZeUnit/docs/how-to-contribute' },
-    //     { title: 'Architecture guide', href: '/ZeUnit/docs/architecture-guide' },
-    //     { title: 'Design principles', href: '/ZeUnit/docs/design-principles' },
+    //     { title: 'How to contribute', href: '/docs/how-to-contribute' },
+    //     { title: 'Architecture guide', href: '/docs/architecture-guide' },
+    //     { title: 'Design principles', href: '/docs/design-principles' },
     //   ],
     // },
   ]
