775bd38622a33b0b175dd7cc2812b5b650086618
Sergei Golos <sergei.golos@gmail.com>
2024-02-14 13:22:52 -0500

    updated the docs to show the current page in the nav panel

--- qwik-docs/src/routes/navigation.ts
+++ qwik-docs/src/routes/navigation.ts
@@ -3,44 +3,44 @@ export const navigation = [
     title: 'Introduction',
     links: [
       { title: 'Overview', href: '/'},
-      { title: 'Getting Started', href: '/docs/getting-started' },
-      { title: 'Test Execution', href: '/docs/test-execution' },
-      { title: 'Testing a Calculator', href: '/docs/testing-a-calculator'}
+      { title: 'Getting Started', href: '/docs/getting-started/' },
+      { title: 'Test Execution', href: '/docs/test-execution/' },
+      { title: 'Testing a Calculator', href: '/docs/testing-a-calculator/' }
     ],
   },
   {
     title: 'Core Concepts',
     links: [
-      { title: 'Class Activation', href: '/docs/core-class-activation' },
-      { title: 'Method Binders', href: '/docs/core-method-binders' },
-      { title: 'Reporting', href: '/docs/core-reporting' },
-      { title: 'Test Suites & Lifecycles', href: '/docs/test-suites-lifecylces' },
+      { title: 'Class Activation', href: '/docs/core-class-activation/' },
+      { title: 'Method Binders', href: '/docs/core-method-binders/' },
+      { title: 'Reporting', href: '/docs/core-reporting/' },
+      { title: 'Test Suites & Lifecycles', href: '/docs/test-suites-lifecylces/' },
     ],
   },
   {
     title: 'Advanced Guides',
     links: [
-      { title: 'Building Class Activators', href: '/docs/building-class-activators' },
-      { title: 'Building Method Binders', href: '/docs/building-method-binders' },
-      { title: 'Custom Suite Lifecycles', href: '/docs/custom-suite-lifecycles' },
-      { title: 'Customizing Reporting', href: '/docs/customizing-reporting' },
+      { title: 'Building Class Activators', href: '/docs/building-class-activators/' },
+      { title: 'Building Method Binders', href: '/docs/building-method-binders/' },
+      { title: 'Custom Suite Lifecycles', href: '/docs/custom-suite-lifecycles/' },
+      { title: 'Customizing Reporting', href: '/docs/customizing-reporting/' },
     ],
   },
   // {
   //   title: 'API reference',
   //   links: [
-  //     { title: 'CacheAdvance.predict()', href: '/docs/cacheadvance-predict' },
-  //     { title: 'CacheAdvance.flush()', href: '/docs/cacheadvance-flush' },
-  //     { title: 'CacheAdvance.revert()', href: '/docs/cacheadvance-revert' },
-  //     { title: 'CacheAdvance.regret()', href: '/docs/cacheadvance-regret' },
+  //     { title: 'CacheAdvance.predict()', href: '/docs/cacheadvance-predict/' },
+  //     { title: 'CacheAdvance.flush()', href: '/docs/cacheadvance-flush/' },
+  //     { title: 'CacheAdvance.revert()', href: '/docs/cacheadvance-revert/' },
+  //     { title: 'CacheAdvance.regret()', href: '/docs/cacheadvance-regret/' },
   //   ],
   // },
   // {
   //   title: 'Contributing',
   //   links: [
-  //     { title: 'How to contribute', href: '/docs/how-to-contribute' },
-  //     { title: 'Architecture guide', href: '/docs/architecture-guide' },
-  //     { title: 'Design principles', href: '/docs/design-principles' },
+  //     { title: 'How to contribute', href: '/docs/how-to-contribute/' },
+  //     { title: 'Architecture guide', href: '/docs/architecture-guide/' },
+  //     { title: 'Design principles', href: '/docs/design-principles/' },
   //   ],
   // },
 ]
