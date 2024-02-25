47acb2f8831af5f254196b2ca702ac3f9fc2ee1f
Sergei Golos <sergei.golos@gmail.com>
2024-02-25 01:14:24 -0500

    reaplaced links with A

--- qwik-docs/src/components/template/header.tsx
+++ qwik-docs/src/components/template/header.tsx
@@ -27,19 +27,19 @@ export default component$(() => {
             <MobileNavigation />
         </div>
         <div class="relative flex flex-grow basis-0 items-center">
-            <Link href="/" aria-label="Home page">
+            <a href="/" aria-label="Home page">
             {/* <Logomark class="h-9 w-9 lg:hidden" />           */}
             <Logo class="hidden h-9 w-auto fill-slate-700 lg:block dark:fill-sky-100 text-2xl" />
-            </Link>
+            </a>
         </div>
         <div class="-my-5 mr-6 sm:mr-8 md:mr-0">
             {/* <Search /> */}
         </div>
         <div class="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
             {/* <ThemeSelector class="relative z-10" /> */}
-            <Link href="https://github.com/bitcobblers/ZeUnit/" target="_blank" class="group" aria-label="ZeUnit repository on GitHub">
+            <a href="https://github.com/bitcobblers/ZeUnit/" target="_blank" class="group" aria-label="ZeUnit repository on GitHub">
             <GitHubIcon class="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
-            </Link>
+            </a>
         </div>
       </div>
     </header>
