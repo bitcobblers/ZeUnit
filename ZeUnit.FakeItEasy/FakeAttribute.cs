ecc27b2b519e9abab54751af1fea20bac5492990
Sergei Golos <sergei.golos@gmail.com>
2024-02-27 16:40:51 -0500

    Created a dynamic fakes composer

--- ZeUnit.FakeItEasy/FakeAttribute.cs
+++ ZeUnit.FakeItEasy/FakeAttribute.cs
@@ -1,4 +1,8 @@
-﻿namespace ZeUnit.FakeItEasy;
+﻿using System.Linq.Expressions;
+using ZeUnit.Composers;
+
+namespace ZeUnit.FakeItEasy;
+
 
 public class FakeAttribute<TType> 
     : ZeComposerAttribute<FakeClassComposer<TType>>
