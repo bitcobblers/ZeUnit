f4a35100c70e0ec0f46100558a06ea5cc21722f9
Sergei Golos <sergei.golos@gmail.com>
2024-02-27 16:41:48 -0500

    code cleanup

--- ZeUnit.FakeItEasy/FakeAttribute.cs
+++ ZeUnit.FakeItEasy/FakeAttribute.cs
@@ -1,12 +1,7 @@
-﻿using System.Linq.Expressions;
-using ZeUnit.Composers;
-
-namespace ZeUnit.FakeItEasy;
-
+﻿namespace ZeUnit.FakeItEasy;
 
 public class FakeAttribute<TType> 
     : ZeComposerAttribute<FakeClassComposer<TType>>
     where TType : class
 {
-
 }
