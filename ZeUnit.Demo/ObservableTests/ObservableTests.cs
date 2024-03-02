4a34367a4910548b385a7a8e8caa440c7351664a
Sergei Golos <sergei.golos@gmail.com>
2024-03-02 17:54:18 -0500

    commended out skipped code (#108)

--- ZeUnit.Demo/ObservableTests/ObservableTests.cs
+++ ZeUnit.Demo/ObservableTests/ObservableTests.cs
@@ -1,28 +1,28 @@
-﻿using System.Reactive.Linq;
+﻿// using System.Reactive.Linq;
 
-namespace ZeUnit.Demo.ObservableTests
-{
-    public class SkippedTests
-    {
-        [Skip("Some Attribute Reason")]
-        public Fact TestIsSkippedByAttribute()
-        {
-            return false;
-        }
+// namespace ZeUnit.Demo.ObservableTests
+// {
+//     public class SkippedTests
+//     {
+//         [Skip("Some Attribute Reason")]
+//         public Fact TestIsSkippedByAttribute()
+//         {
+//             return false;
+//         }
 
-        public Fact TestIsSkippedByCode()
-        {
-            return Is.Skipped("Some Runtime Reason");
-        }
-    }
+//         public Fact TestIsSkippedByCode()
+//         {
+//             return Is.Skipped("Some Runtime Reason");
+//         }
+//     }
 
-    public class ObservableTests
-    {
-        public IObservable<Fact> EnumerableTestRun()
-        {
-            return Observable.Interval(new TimeSpan(1000))
-                .Take(5)
-                .Select(i => i.Is(i));
-        }
-    }
-}
+//     public class ObservableTests
+//     {
+//         public IObservable<Fact> EnumerableTestRun()
+//         {
+//             return Observable.Interval(new TimeSpan(1000))
+//                 .Take(5)
+//                 .Select(i => i.Is(i));
+//         }
+//     }
+// }
