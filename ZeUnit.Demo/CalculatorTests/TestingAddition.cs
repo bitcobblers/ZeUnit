b7ef64d1b5522963961cda0faa3e6ab8224b2980
Sergei Golos <sergei.golos@gmail.com>
2024-03-02 18:26:48 -0500

    C# 12 syntax

--- ZeUnit.Demo/CalculatorTests/TestingAddition.cs
+++ ZeUnit.Demo/CalculatorTests/TestingAddition.cs
@@ -13,18 +13,18 @@ public class TestingAddition
     public Fact PassingSingleValueResultWithTheValue()
     {
         var addition = new AddOperation();
-        return addition.Apply(0, new[] { 1d })!.Value.Is(1d);
+        return addition.Apply(0, [1d])!.Value.Is(1d);
     }
 
     public Fact PassingTwoValuesResultWithTheSum()
     {
         var addition = new AddOperation();
-        return addition.Apply(0, new[] { 1d, 2d })!.Value.Is(3d);
+        return addition.Apply(0, [1d, 2d])!.Value.Is(3d);
     }
 
     public Fact PassingManyValuesResultWithTheSum()
     {
         var addition = new AddOperation();
-        return addition.Apply(0, new[] { 1d, 2d, 3d, 4d })!.Value.Is(10d);
+        return addition.Apply(0, [1d, 2d, 3d, 4d])!.Value.Is(10d);
     }
 }
